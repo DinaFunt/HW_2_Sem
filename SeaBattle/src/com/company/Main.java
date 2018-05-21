d5433a0d210b73a2d599943290ea65d412ffdfac
Dina Funt <dina.funt@gmail.com>
2018-05-21 10:44:58 +0300

    Ioc added

--- SeaBattle/src/com/company/Main.java
+++ SeaBattle/src/com/company/Main.java
@@ -1,12 +1,15 @@
 package com.company;
 
 import java.util.*;
+import org.picocontainer.DefaultPicoContainer;
+import org.picocontainer.MutablePicoContainer;
 
 public class Main {
 
     public static void main(String[] Args) {
 
-        boolean a;
+        MutablePicoContainer Pica = new DefaultPicoContainer();
+
         Scanner in = new Scanner(System.in);
         ComputerPlayer Opponent;
 
@@ -16,77 +19,25 @@ public class Main {
         ServiceLoader<ComputerPlayer> serviceLoader =
                 ServiceLoader.load(ComputerPlayer.class);
 
-
-        for (ComputerPlayer cpService : serviceLoader) {
-            cpService.Print();
-        }
-
         switch (TypeOfOpponent) {
             case 1:
-                Opponent = new RandomPlayer();
+                Pica.addComponent(RandomPlayer.class);
                 break;
             case 2:
-                Opponent = new Smart();
+                Pica.addComponent(Smart.class);
                 break;
             case 3:
-                Opponent = serviceLoader.findFirst().get();
+
+                Pica.addComponent(serviceLoader.findFirst());
                 break;
             default:
                 System.out.print("We're sorry. You enter wrong number so your opponent'll be stupid. :\\ \n");
-                Opponent = new RandomPlayer();
+                Pica.addComponent(RandomPlayer.class);
         }
 
-        Opponent.SetShips();
-        User User = new User();
-        User.SetShips();
+        Pica.addComponent(ProcessGame.class);
+        ProcessGame pg = (ProcessGame) Pica.getComponent(ProcessGame.class);
 
-        System.out.print("You start first!\n");
-
-        do {
-            System.out.print("Enter X, Y.\n");
-            User.NextStep();
-            if (User.Count == 0) {
-                break;
-            }
-            a = Opponent.Hit(User.X, User.Y);
-            User.Result(a, User.X, User.Y);
-
-            while (a) {
-                System.out.print("You hit the ship. Enter X, Y again.\n");
-                User.NextStep();
-                if (User.Count == 0) {
-                    break;
-                }
-                a = Opponent.Hit(User.X, User.Y);
-                User.Result(a, User.X, User.Y);
-            }
-
-            Opponent.NextStep();
-            if (Opponent.Count == 0) {
-                break;
-            }
-            a = User.Hit(Opponent.X, Opponent.Y);
-            Opponent.Result(a, Opponent.X, Opponent.Y);
-            while (a) {
-                Opponent.Flag++;
-                Opponent.NextStep();
-                if (Opponent.Count == 0) {
-                    break;
-                }
-                a = User.Hit(Opponent.X, Opponent.Y);
-                Opponent.Result(a, Opponent.X, Opponent.Y);
-            }
-
-            System.out.println();
-
-        }
-        while (Opponent.Count != 0 && User.Count != 0);
-
-        if (Opponent.Count == 0) {
-            System.out.print("You lose!\n");
-        }
-        else {
-            System.out.print("You win!\n");
-        }
+        pg.Game();
     }
 }
